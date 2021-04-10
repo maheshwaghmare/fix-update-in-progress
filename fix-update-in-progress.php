966d60be996b4239b98e1771e8d23ba371e81c86
Mahesh Waghmare <mwaghmare7@gmail.com>
2021-04-10 12:11:30 +0530

    Update fix-update-in-progress.php

--- fix-update-in-progress.php
+++ fix-update-in-progress.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://profiles.wordpress.org/mahesh901122/
  * Author: Mahesh M. Waghmare
  * Author URI: https://maheshwaghmare.com/
- * Version: 1.0.0
+ * Version: 1.0.1
  * License: GNU General Public License v2.0
  * Text Domain: fix-update-in-process
  *
@@ -13,7 +13,7 @@
  */
 
 // Set constants.
-define( 'FIX_UPDATE_IN_PROCESS_VER', '1.0.0' );
+define( 'FIX_UPDATE_IN_PROCESS_VER', '1.0.1' );
 define( 'FIX_UPDATE_IN_PROCESS_FILE', __FILE__ );
 define( 'FIX_UPDATE_IN_PROCESS_BASE', plugin_basename( FIX_UPDATE_IN_PROCESS_FILE ) );
 define( 'FIX_UPDATE_IN_PROCESS_DIR', plugin_dir_path( FIX_UPDATE_IN_PROCESS_FILE ) );
